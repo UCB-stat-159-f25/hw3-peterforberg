ffaf610d128a382fcd05590667345cb09fd213be
Peter Forberg <peterforberg@berkeley.edu>
2025-11-07 08:36:58 +0000

    added tests for readligo.py and updated environment.yml to include pytest

--- ligotools/ligotools/tests/test_readligo.py
+++ ligotools/ligotools/tests/test_readligo.py
@@ -1,8 +1,21 @@
 from ligotools import readligo as rl
+import pytest
+import os
 
-def test_ligo():
-    try:
-        data = rl.loaddata("data/H-H1_LOSC_4_V2-1126259446-32.hdf5")
-        assert data != None
-    except:
-        raise 
+# This test provides an empty .hdf5 file, which should output `None` if loaded using loaddata
+def test_loaddata_length():
+    fakefile = "test.hdf5"
+    fakecontent = ""
+    with open(fakefile, 'w') as file_object:
+        file_object.write(fakecontent)
+
+    value1, value2, value3 = rl.loaddata("test.hdf5")
+    assert value1 == None
+
+    os.remove(fakefile)
+
+# This test provides a list instead of a dict to `dq_channel_to_seglist()`, which raises a TypeError
+def test_dq_channel_to_seglist():
+    fakedict = []
+    with pytest.raises(TypeError):
+        rl.dq_channel_to_seglist(fakedict)
